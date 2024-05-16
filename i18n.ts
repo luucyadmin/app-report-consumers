e9ad5d7bc58d9bb0bbeee50351f64433a4b06472
Thorben Westerhuys <thorben.westerhuys@luucy.ch>
2024-05-16 11:43:34 +0200

    LU-7064 fix crowdin multi-repo setup

--- i18n.ts
+++ i18n.ts
@@ -1,6 +1,6 @@
-import de from "./i18n/de";
-import en from "./i18n/en";
-import fr from "./i18n/fr";
+import de from "./i18n/report-consumers.de";
+import en from "./i18n/report-consumers.en";
+import fr from "./i18n/report-consumers.fr";
 
 type I18nParams = {[key: string]: any};
 type I18nFunction = {
---
+++ i18n/report-consumers.de.ts
@@ -0,0 +1,13 @@
+export default {
+  Loading: 'Chargement',
+  Move_around_to_select: 'Move around to select districts',
+  Clear_Log: 'Clear Log',
+  District_Title: 'District {{id}}',
+  Nature_Report_Info: 'Get a nature report about this district to learn more about the environment influences and data',
+  Restore_Log: 'Restore for {{id}}: {{token}}',
+  Download_Report_Luucy: 'Download Report (open Luucy)',
+  Buy_Report: 'Buy Report',
+  Requesting_Purchase: 'Requesting Purchase',
+  Purchase_Response: 'Purchase Response: {{token}}',
+  Download_Report: 'Download Report',
+};
