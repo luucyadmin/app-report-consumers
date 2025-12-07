665937304b974bafe31d8f5911194f69086f806b
Jakub Kucera <kucera@zensys.cz>
2025-12-08 11:11:19 +1300

    LU-7982 - fix activation

--- i18n/report-consumers.en.ts
+++ i18n/report-consumers.en.ts
@@ -1,6 +1,6 @@
 export default {
   Loading: 'Chargement',
-  Move_around_to_select: 'Move around to select districts',
+  Move_around_to_select: 'Move around to select districtsX',
   Clear_Log: 'Clear Log',
   District_Title: 'District {{id}}',
   Nature_Report_Info: 'Get a nature report about this district to learn more about the environment influences and data',
--- plugin.ts
+++ plugin.ts
@@ -1,58 +1,68 @@
 import { District } from "./district";
-import i18n from './i18n';
+import i18n from "./i18n";
 
 const section = ui.createProjectPanelSection();
 section.add(new ui.Paragraph(i18n.Move_around_to_select()));
 
-const initialDistrict = new District(map.location.center);
+export const districts: District[] = [];
 
-export const districts: District[] = [initialDistrict];
-
-export let activeDistrict = initialDistrict;
-activeDistrict.update();
-
-let districtSection = initialDistrict.render();
-section.add(districtSection);
-
-const marker = new map.Marker(map.location.center.flattenedCopy(), Color.black);
-marker.overlayBuildings = true;
+export let activeDistrict; // = initialDistrict;
+let districtSection;
+let marker;
 
 const log = new ui.Container();
 section.add(log);
 
 const clearLogButton = new ui.Button(i18n.Clear_Log(), () => {
-    for (let child of log.children.slice(1)) {
-        log.remove(child);
-    }
+  for (let child of log.children.slice(1)) {
+    log.remove(child);
+  }
 });
 
 log.add(clearLogButton);
 
-export const createLog = message => {
-    log.insertAfter(new ui.Note(ui.info, message), clearLogButton);
+export const createLog = (message) => {
+  log.insertAfter(new ui.Note(ui.info, message), clearLogButton);
 };
+let subscription;
+section.onOpen.subscribe(() => {
+  marker = new map.Marker(map.location.center.flattenedCopy(), Color.black);
+  marker.overlayBuildings = true;
 
-map.location.onCenterChange.subscribe(position => {
-    marker.move(position.flattenedCopy());
+  const initialDistrict = new District(map.location.center);
+  districts.push(initialDistrict);
+  activeDistrict = initialDistrict;
+  districtSection = initialDistrict.render();
+  section.add(districtSection);
 
-    let district = districts.find(district => district.isInside(position));
+  subscription = map.location.onCenterChange.subscribe((position) => {
+    if (marker) {
+      marker.move(position.flattenedCopy());
+    }
+    let district = districts.find((district) => district.isInside(position));
 
     if (!district) {
-        district = new District(position);
+      district = new District(position);
 
-        districts.push(district);
+      districts.push(district);
     }
 
-    if (district != activeDistrict) {
-        const deactivatedDistrict = activeDistrict;
-        activeDistrict = district;
+    if (district.id != activeDistrict.id) {
+      activeDistrict = district;
 
-        deactivatedDistrict.update();
-        district.update();
+      district.update();
 
-        section.remove(districtSection);
+      section.remove(districtSection);
 
-        districtSection = district.render();
-        section.insertBefore(districtSection, log);
+      districtSection = district.render();
+      section.insertBefore(districtSection, log);
     }
+  });
+});
+
+section.onClose.subscribe(() => {
+  districts.forEach((district) => district.area?.remove());
+  section.remove(districtSection);
+  marker.remove();
+  subscription.unsubscribe();
 });
